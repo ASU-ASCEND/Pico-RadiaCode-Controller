bda6a3be3e15604abab33475373d150d2c2a089c
Tyler Nielsen <tylernielsenn@gmail.com>
2025-10-09 14:19:51 -0700

    making scanner more useful

--- main.py
+++ main.py
@@ -8,15 +8,28 @@ print(machine.freq())
 led = machine.Pin("LED", machine.Pin.OUT)
 
 
-async def bt_scan():
+async def bt_scan_print():
   async with aioble.scan(duration_ms=5000) as scanner:
     async for result in scanner:
       print(result, result.name(), result.rssi, result.services())
 
+async def bt_scan():
+  results = []
+  seen_addr = set()
+  async with aioble.scan(duration_ms=5000, interval_us=30000, window_us=30000, active=True) as scanner:
+    async for result in scanner: 
+      if result.name() is not None and result.name() != "Seos" and result.device.addr not in seen_addr:
+        results.append(result)
+        seen_addr.add(result.device.addr)
+
+  return results
 
 async def main():
   while True:
-    await bt_scan()
+    results = await bt_scan()
+    print("\nScan Complete, Results: ")
+    for res in results:
+      print(res.name(), res.device.addr_hex())
     time.sleep(10)
 
 

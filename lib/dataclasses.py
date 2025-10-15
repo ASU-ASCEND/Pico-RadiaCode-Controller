aeb016154f6b24ad3117fae1b298180ec8d7d9f5
Tyler Nielsen <tylernielsenn@gmail.com>
2025-10-14 23:34:16 -0700

    down to errors on bluetooth now

--- lib/dataclasses.py
+++ lib/dataclasses.py
@@ -1,19 +1,20 @@
 # type: ignore
-def dataclass(*, init=True, repr=True, eq=True):
+def dataclass(_cls=None, *, init=True, repr=True, eq=True):
+  print(type(_cls))
   def wrap(cls):
-    fields = cls.__annotations__
+    fields = getattr(cls, "__annotations__", {}) #cls.__annotations__
 
     if init:
       def __init__(self, **kwargs):
         for name, value in kwargs.items():
           setattr(self, name, value)
-
       cls.__init__ = __init__
 
     if repr: 
       def __repr__(self):
         values = ", ".join(f"{k}={getattr(self, k)!r}" for k in fields)
         return f"{cls.__name__}({values})"
+      cls.__repr__ = __repr__
 
     if eq:
       def __eq__(self, other):
@@ -22,5 +23,11 @@ def dataclass(*, init=True, repr=True, eq=True):
             return False
 
         return True
+      cls.__eq__ = __eq__
+    
+    return cls
 
-  return wrap
+  if _cls is None:
+    return wrap
+  else:
+    return wrap(_cls)
